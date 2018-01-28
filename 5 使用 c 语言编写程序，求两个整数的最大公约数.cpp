3f69119395943269e8d3f03ca30b5498d901bd5f
NiuBenn <33975700+NiuBenn@users.noreply.github.com>
2018-01-28 21:17:28 +0800

    5 使用 c 语言编写程序，求两个整数的最大公约数
    使用 c 语言编写程序，求两个整数的最大公约数

---
+++ 5 使用 c 语言编写程序，求两个整数的最大公约数.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+	int a, b, c;
+	printf("please enter two number:");
+	scanf("%d%d", &a, &b);
+	while (b != 0)
+	{
+		c = a%b;
+		a = b;
+		b = c;
+	}
+	printf("Greatest common divisor is:%d\n", a);
+	system("pause");
+	return 0;
+}
